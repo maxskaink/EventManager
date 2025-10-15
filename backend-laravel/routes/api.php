2900dff38ba8bb65f562d876067c138f05e5595b
Crownyny <juliandavidm@unicauca.edu.co>
2025-10-14 23:13:13 -0500

    Test of route auth, succesful

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('auth', [AuthController::class, 'redirectToAuth']);
 #Route::get('auth/callback', [AuthController::class, 'handleAuthCallback']);
-Route::get('user', [AuthController::class, 'user']);
+Route::middleware('auth:sanctum')->get('user', [AuthController::class, 'user']);
 Route::post('/auth/callback', [AuthController::class, 'handleGoogleCallback']);
