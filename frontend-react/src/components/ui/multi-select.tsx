f559baa0db9f8a5ad1fb6d72273ddc98460241bf
frdy <fredyanaya@unicauca.edu.co>
2025-12-11 13:14:16 -0500

    fix: multi-select

--- frontend-react/src/components/ui/multi-select.tsx
+++ frontend-react/src/components/ui/multi-select.tsx
@@ -47,75 +47,80 @@ export function MultiSelect({
   const selectables = options.filter((option) => !selected.includes(option.value));
 
   return (
-    <Command onKeyDown={(e) => {
-      if (e.key === "Backspace" && !inputValue && selected.length > 0) {
-        onChange(selected.slice(0, -1));
-      }
-      if (e.key === "Escape") {
-        inputRef.current?.blur();
-      }
-    }} className={`overflow-visible bg-transparent ${className}`}>
-      <div
-        className="group border border-input px-3 py-2 text-sm ring-offset-background rounded-md focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2"
-      >
-        <div className="flex gap-1 flex-wrap">
-          {selectedOptions.map((option) => (
+    <div className={className}>
+        <Command onKeyDown={(e) => {
+        if (e.key === "Backspace" && !inputValue && selected.length > 0) {
+            onChange(selected.slice(0, -1));
+        }
+        if (e.key === "Escape") {
+            inputRef.current?.blur();
+        }
+        }} className="overflow-visible bg-transparent">
+        <div
+            className="group border border-input px-3 py-2 text-sm ring-offset-background rounded-md focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2"
+        >
+            <div className="flex gap-1 flex-wrap">
+            <CommandPrimitive.Input
+                ref={inputRef}
+                value={inputValue}
+                onValueChange={setInputValue}
+                onBlur={() => setOpen(false)}
+                onFocus={() => setOpen(true)}
+                placeholder={placeholder}
+                className="bg-transparent outline-none placeholder:text-muted-foreground flex-1"
+            />
+            </div>
+        </div>
+        <div className="relative mt-2">
+            {open && selectables.length > 0 ? (
+            <div className="absolute w-full z-10 top-0 rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
+                <CommandList>
+                    <CommandGroup className="h-full overflow-auto text-slate-800">
+                        {selectables.map((option) => (
+                        <CommandItem
+                            key={option.value}
+                            onMouseDown={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                            }}
+                            onSelect={() => {
+                                setInputValue("");
+                                onChange([...selected, option.value]);
+                            }}
+                            className="cursor-pointer"
+                        >
+                            {option.label}
+                        </CommandItem>
+                        ))}
+                    </CommandGroup>
+                </CommandList>
+            </div>
+            ) : null}
+        </div>
+        </Command>
+
+        <div className="flex gap-1 flex-wrap mt-3">
+        {selectedOptions.map((option) => (
             <Badge key={option.value} variant="secondary">
-              {option.label}
-              <button
+            {option.label}
+            <button
                 className="ml-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
                 onKeyDown={(e) => {
-                  if (e.key === "Enter") {
+                if (e.key === "Enter") {
                     handleUnselect(option.value);
-                  }
+                }
                 }}
                 onMouseDown={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
+                e.preventDefault();
+                e.stopPropagation();
                 }}
                 onClick={() => handleUnselect(option.value)}
-              >
+            >
                 <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
-              </button>
+            </button>
             </Badge>
-          ))}
-          <CommandPrimitive.Input
-            ref={inputRef}
-            value={inputValue}
-            onValueChange={setInputValue}
-            onBlur={() => setOpen(false)}
-            onFocus={() => setOpen(true)}
-            placeholder={placeholder}
-            className="bg-transparent outline-none placeholder:text-muted-foreground flex-1"
-          />
+        ))}
         </div>
-      </div>
-      <div className="relative mt-2">
-        {open && selectables.length > 0 ? (
-          <div className="absolute w-full z-10 top-0 rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
-            <CommandList>
-                <CommandGroup className="h-full overflow-auto text-slate-800">
-                    {selectables.map((option) => (
-                    <CommandItem
-                        key={option.value}
-                        onMouseDown={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                        }}
-                        onSelect={() => {
-                            setInputValue("");
-                            onChange([...selected, option.value]);
-                        }}
-                        className="cursor-pointer"
-                    >
-                        {option.label}
-                    </CommandItem>
-                    ))}
-                </CommandGroup>
-            </CommandList>
-          </div>
-        ) : null}
-      </div>
-    </Command>
+    </div>
   );
 }
